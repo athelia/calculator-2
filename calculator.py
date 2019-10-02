32e50c39dd65714047b41bd244f7a7e6b1bb02a3
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-02 16:39:56 -0700

    Renamed integerize to make_float and added function calculate.

--- calculator.py
+++ calculator.py
@@ -12,10 +12,35 @@ def get_user_input():
     user_input = user_input.split()
     return user_input
 
-def integerize(user_input):
+def make_float(user_input):
     for i in range(1,len(user_input)):
         user_input[i] = float(user_input[i])
         i += 1
     return user_input
+
+def calculate(user_input):
+    which_operation = user_input[0]
+    num1 = user_input[1]
+    if len(user_input)>2:
+        num2 = user_input[2]
+
+    if which_operation == "+":
+        add(num1, num2)
+    elif which_operation == "-":
+        subtract(num1, num2)
+    elif which_operation == "*":
+        multiply(num1, num2)
+    elif which_operation == "/":
+        divide(num1, num2)
+    elif which_operation == "square":
+        square(num1)
+    elif which_operation == "cube":
+        cube(num1)
+    elif which_operation == "pow":
+        power(num1, num2)
+    elif which_operation == "mod":
+        mod(num1, num2)
+    elif which_operation == "q":
+        return
 
 #print(get_user_input())
