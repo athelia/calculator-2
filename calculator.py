ad2f7bb3823f71b01823e897632b13a823b32146
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-02 16:20:17 -0700

    Added functions get_user_input and integerize

--- calculator.py
+++ calculator.py
@@ -7,4 +7,15 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# Your code goes here
+def get_user_input():
+    user_input = input()
+    user_input = user_input.split()
+    return user_input
+
+def integerize(user_input):
+    for i in range(1,len(user_input)):
+        user_input[i] = float(user_input[i])
+        i += 1
+    return user_input
+
+#print(get_user_input())
